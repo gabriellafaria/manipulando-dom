0bc82fdabfad25fdde9f5b394f1f8a55889e8365
GabriellaFaria <gabisouf@gmail.com>
2022-05-25 20:48:22 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
 const body = document.querySelector('body');
 const button = document.getElementById('myButton');
-const h1 = document.getElementsByTagName
-
-/* function changes() {
-    changeMode();
-    changeText();
-} */
+const h1 = document.querySelector('h1');
+const items = document.querySelectorAll('.item');
 
 function changeMode() {
-    button.classList.toggle('button-dark')
-
-  }
-
-/* function changeText() {
-const dark = 'Dark Mode';
-const light = 'Light Mode';
-    if (body.classList.contains(body-dark)) {
-        button.innerHTML = light;
-    } else { button.innerHTML = light; }
+    button.classList.toggle('button-dark');
+    body.classList.toggle('body-dark');
+    h1.classList.toggle('title-dark');
+    for (let i of items) {
+        i.classList.toggle('item-dark');
+        i.classList.toggle('container-dark');
+    }
 }
 
 
-button.addEventListener('click', changes); */
+/* Aqui nos "importamos o conteudo como objetos"*/
+
+
+/* let objeto1 = {
+    title: 'O tigre',
+    img: '',
+    descricao: 'lorem ips'
+}
+ */
