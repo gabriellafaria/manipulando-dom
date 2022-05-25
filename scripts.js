2f0281b0aab118dc7df57b44a8498f5ef6979374
GabriellaFaria <gabisouf@gmail.com>
2022-05-25 07:28:17 -0300

    update

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+const button = document.getElementById('myButton');
+const h1 = document.getElementsByTagName
+
+/* function changes() {
+    changeMode();
+    changeText();
+} */
+
+function changeMode() {
+    button.classList.toggle('button-dark')
+
+  }
+
+/* function changeText() {
+const dark = 'Dark Mode';
+const light = 'Light Mode';
+    if (body.classList.contains(body-dark)) {
+        button.innerHTML = light;
+    } else { button.innerHTML = light; }
+}
+
+
+button.addEventListener('click', changes); */
